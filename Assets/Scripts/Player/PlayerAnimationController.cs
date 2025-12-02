c5942ae4820119c0e13a0976e43f173e90995654
Elise <elizerosecampestrini@gmail.com>
2025-12-02 17:55:47 +0100

    cinemachine essayé (ca marche) mais a confirmer

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 public class PlayerAnimationController : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
-    
+    [SerializeField] private PlayerInputHandler _playerInputHandler;
 
-    public void Walk(bool isWalking)
+    private void Start()
     {
-        _animator.SetBool("isWalking", isWalking);
+        // abonnement
+        _playerInputHandler.OnMove += SetWalkAnimation;
+        _playerInputHandler.OnSprint += SetRunAnimation;
     }
 
-    public void Run(bool isRunning)
+    private void OnDestroy()
+    {
+        _playerInputHandler.OnMove -= SetWalkAnimation;
+        _playerInputHandler.OnSprint -= SetRunAnimation;
+    }
+
+    public void SetWalkAnimation(Vector2 moveInputDirection)
+    {
+        _animator.SetBool("isWalking", moveInputDirection != Vector2.zero);
+    }
+
+    public void SetRunAnimation(bool isRunning)
     {
         _animator.SetBool("isRunning", isRunning);
     }
